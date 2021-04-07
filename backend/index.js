9518367304813fe0e006e45003b2cbe9a96a5848
DenisAfa <denisafanasevm92@mail.ru>
2021-04-07 13:13:53 +0300

    feat: add animation chat title

--- backend/index.js
+++ backend/index.js
@@ -23,12 +23,12 @@ io.on("connect", (socket) => {
     socket.join(user.room);
 
     socket.emit("message", {
-      user: "admin",
+      user: "info",
       text: `${user.name}, welcome to room ${user.room}.`,
     });
     socket.broadcast
       .to(user.room)
-      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+      .emit("message", { user: "info", text: `${user.name} has joined!` });
 
     io.to(user.room).emit("roomData", {
       room: user.room,
@@ -54,7 +54,7 @@ io.on("connect", (socket) => {
 
     if (user) {
       io.to(user.room).emit("message", {
-        user: "Admin",
+        user: "info",
         text: `${user.name} has left.`,
       });
       io.to(user.room).emit("roomData", {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,13 @@ import { Context } from "./context";
 
 const AUTH_PATH = "/";
 export const CHAT_PATH = "/chat";
+const APP_TITLE = "Chat App";
 
 const App = () => {
   const { hasUserAuth } = useContext(Context);
   return (
     <main className="main-wrapper">
+      <h1 className="main__title">{APP_TITLE}</h1>
       <Route path={AUTH_PATH} exact render={() => <Auth />} />
       <Route path={CHAT_PATH} render={() => <Chat />} />
       {/*<Route path={CHAT_PATH} render={() => (hasUserAuth ? <Chat /> : <Auth />)} />*/}
